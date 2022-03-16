6ff5815d637e13e431915b971e78a296af0708f6
Felisiak <P50068551@capita.co.uk>
2022-03-16 15:08:41 +0000

    Double tap!

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-import PinchGesture from './src/components/PinchGesture';
+import React, { useCallback, useRef } from 'react';
+import {
+  Dimensions,
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from 'react-native';
+import {
+  GestureHandlerRootView,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import DoubleTap from './src/components/DoubleTap';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PinchGesture />
+      <DoubleTap />
       <StatusBar style='dark' />
     </GestureHandlerRootView>
   );
---
+++ src/components/DoubleTap/index.tsx
@@ -0,0 +1,107 @@
+import {
+  StyleSheet,
+  View,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+import React, { useRef } from 'react';
+import { TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+const DoubleTap = () => {
+  const scale = useSharedValue(0);
+  const opacity = useSharedValue(1);
+  const doubleTapRef = useRef();
+
+  const onDoubleTap = () => {
+    scale.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        scale.value = withDelay(500, withSpring(0));
+      }
+    });
+  };
+
+  const onSingleTap = () => {
+    opacity.value = withTiming(0, undefined, (isFinished) => {
+      if (isFinished) {
+        opacity.value = withDelay(500, withTiming(1));
+      }
+    });
+  };
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: Math.max(scale.value, 0) }],
+    };
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <TapGestureHandler waitFor={doubleTapRef} onActivated={onSingleTap}>
+        <TapGestureHandler
+          ref={doubleTapRef}
+          numberOfTaps={2}
+          onActivated={onDoubleTap}
+        >
+          <View>
+            <ImageBackground
+              style={styles.image}
+              source={require('../../../assets/image.jpeg')}
+            >
+              <AnimatedImage
+                style={[styles.heartImage, rStyle]}
+                source={require('../../../assets/heart.png')}
+              ></AnimatedImage>
+            </ImageBackground>
+          </View>
+        </TapGestureHandler>
+      </TapGestureHandler>
+
+      <Animated.Text style={[styles.turtles, rTextStyle]}>
+        🐢🐢🐢🐢
+      </Animated.Text>
+    </View>
+  );
+};
+
+export default DoubleTap;
+
+const { width: SIZE } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: SIZE,
+    height: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heartImage: {
+    height: 250,
+    width: 250,
+  },
+  turtles: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 0,
+  },
+});
